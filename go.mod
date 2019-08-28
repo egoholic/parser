11ecc0806f733f6d1931f40983120b953d7973b7
Volodymyr Melnyk <vmelnyk@tradetracker.com>
2019-08-28 14:44:08 +0300

    extends context a little bit

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/egoholic/juggler v0.0.0-20190418164904-fdd9100e6441
 	github.com/onsi/ginkgo v1.9.0
 	github.com/onsi/gomega v1.6.0
+	golang.org/dl v0.0.0-20190821202747-15c2a0da8b07 // indirect
 )
--- grammar/graph/traverser/context/context.go
+++ grammar/graph/traverser/context/context.go
@@ -1,9 +1,44 @@
 package context
 
 import (
+	"sync"
+
 	"github.com/egoholic/parser/grammar/graph/vertex"
 )
 
 type Context struct {
-	possibleVertice []*vertex.Vertex
+	wg       *sync.WaitGroup
+	vertices []*vertex.Vertex
+}
+
+func New(wg *sync.WaitGroup, vertices []*vertex.Vertex) *Context {
+	return &Context{
+		wg:       wg,
+		vertices: vertices,
+	}
+}
+
+func (c *Context) Exec() error {
+	c.wg.Add(1)
+	defer c.wg.Done()
+	for _, v := range c.vertices {
+		c.wg.Add(1)
+		go func(wg *sync.WaitGroup, v *vertex.Vertex) {
+			defer wg.Done()
+		}(c.wg, v)
+	}
+}
+
+func (c *Context) parseWith(wg *sync.WaitGroup, v *vertex.Vertex) {
+	defer wg.Done()
+
+}
+
+func (c *Context) switchToParseEndingMode(endingVertices []*vertex.Vertex) {
+	for _, v := range endingVertices {
+		c.wg.Add(1)
+		go func(wg *sync.WaitGroup, v *vertex.Vertex) {
+			defer wg.Done()
+		}(c.wg, v)
+	}
 }
